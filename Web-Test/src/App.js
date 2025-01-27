bc5cbb26924a209192a192d5e2ae7b02257dbc44
Jnckk <riskicahgendeng@gmail.com>
2025-01-27 13:35:46 +0700

    Privacy Policy added

--- Web-Test/src/App.js
+++ Web-Test/src/App.js
@@ -1,11 +1,20 @@
+import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import ChatBot from "./Pages/ChatBot";
+import PrivacyPolicy from "./Pages/PrivacyPolicy"; // Import halaman PrivacyPolicy
 
 const App = () => {
   return (
-    <div style={{ height: "100vh" }}>
-      <ChatBot />
-    </div>
+    <Router>
+      <div style={{ height: "100vh" }}>
+        <Routes>
+          <Route path="/" element={<ChatBot />} /> {/* Halaman utama */}
+          <Route path="/privacy-policy" element={<PrivacyPolicy />} />{" "}
+          {/* Halaman Privacy Policy */}
+        </Routes>
+      </div>
+    </Router>
   );
 };
 
