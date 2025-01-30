ce8bef0bb47377bd5653d818260f1f2f645ea81a
Jnckk <riskicahgendeng@gmail.com>
2025-01-30 14:20:18 +0700

    `Revert "Remove PrivacyPolicy import" and restore original import`

--- Web-Test/src/App.js
+++ Web-Test/src/App.js
@@ -2,16 +2,15 @@ import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import ChatBot from "./Pages/ChatBot";
-import PrivacyPolicy from "./Pages/PrivacyPolicy"; // Import halaman PrivacyPolicy
+import PrivacyPolicy from "./Pages/PrivacyPolicy";
 
 const App = () => {
   return (
     <Router>
       <div style={{ height: "100vh" }}>
         <Routes>
-          <Route path="/" element={<ChatBot />} /> {/* Halaman utama */}
-          <Route path="/privacy-policy" element={<PrivacyPolicy />} />{" "}
-          {/* Halaman Privacy Policy */}
+          <Route path="/" element={<ChatBot />} />
+          <Route path="/privacy-policy" element={<PrivacyPolicy />} />
         </Routes>
       </div>
     </Router>
